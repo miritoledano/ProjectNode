836c48037619194b1e02a74f316c945b01565824
miritoledano <mt0548553246@gmail.com>
2024-03-08 16:12:03 +0200

    תיאור השינויים

--- db/connectToDB.js
+++ db/connectToDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToDB = async () => {
     try {
-        let connect = await mongoose.connect(process.env.PORT||"mongodb+srv://yael0527623744:3QFK7H5aQL5dp354@miri.hjtrrga.mongodb.net/")
+        let connect = await mongoose.connect(process.env.DB_URI||"mongodb+srv://yael0527623744:3QFK7H5aQL5dp354@miri.hjtrrga.mongodb.net/")
         console.log("mongo db connected")
     }
     catch (err) {
